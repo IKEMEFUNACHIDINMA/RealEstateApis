6b8fe6acc84679bd21067ae053a3c4892443f5d3
IKEMEFUNA CHIDINMA <ikemefunachidinma123@gmail.com>
2025-08-11 23:29:01 +0100

    Initial commit

---
+++ src/main/java/com/example/realestateapis/RealEstateApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.realestateapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realestateapis/RealEstateApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realestateapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
