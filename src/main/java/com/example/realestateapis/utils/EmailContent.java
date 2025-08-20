c5d42d4c8e64fa633e115791af8162bb9632e748
IKEMEFUNA CHIDINMA <ikemefunachidinma123@gmail.com>
2025-08-20 14:31:07 +0100

    trial

--- src/main/java/com/example/realestateapis/utils/EmailContent.java
+++ src/main/java/com/example/realestateapis/utils/EmailContent.java
@@ -3,7 +3,7 @@ package com.example.realestateapis.utils;
 public class EmailContent {
     public static String registrationEmail(){
         return "<p> Dear user, </p>"+
-                "<p> Your registration to Enulux has been successful! </p>";
+                "<p> Your registration to Enuluxe has been successful! </p>";
     }
 
     public static String bookingEmail(){
