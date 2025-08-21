53a3d34ac013b7ca2abd93b90849f78b51fb7c07
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-08-21 13:28:31 +0100

    Revert "Configuration"
    This reverts commit c3ecfe4a5596795177fc85cf4565009b50a0acbe.

--- src/main/java/com/example/realestateapis/RealEstateApisApplication.java
+++ src/main/java/com/example/realestateapis/RealEstateApisApplication.java
@@ -12,7 +12,6 @@ public class RealEstateApisApplication {
 //        System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
 //        System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
 //        System.setProperty("PAYSTACK_SECRET_KEY", dotenv.get("PAYSTACK_SECRET_KEY"));
-
         SpringApplication.run(RealEstateApisApplication.class, args);
         System.out.println("Real Estate Api is running!");
     }
